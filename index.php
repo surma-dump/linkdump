073e09a690d64f87f68f28976372fa3f923d08fd
surma <surma@eeearch.(none)>
2009-03-19 12:57:55 +0100

    Outsourced DB password *cough*

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?
 	$token="ff1cdad3cc8bab54045f221ad1a936ee";
-	$sql = mysql_connect ("localhost", "surma", "su4wi");
+	include ("./config.inc");
+	$sql = mysql_connect ("localhost", $DB_USER, $DB_PASS);
 	mysql_select_db("surma");
 
 	$data="" ;
